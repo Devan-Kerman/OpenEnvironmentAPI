6dbdbbe3c7e4b9aa7cf70afdc0b97ababb4646aa
Devan-Kerman <dev.sel20@gmail.com>
2021-01-10 19:02:24 -0600

    Initial commit

---
+++ src/main/java/net/devtech/openenvironmentapi/OpenEnvironmentAPI.java
@@ -0,0 +1,10 @@
+package net.devtech.openenvironmentapi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OpenEnvironmentAPI implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
